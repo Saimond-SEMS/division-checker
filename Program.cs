3506432ca90e6bc162641d2eb9c29508c12d414f
Saimond-SEMS <exactsameghost@gmail.com>
2025-02-13 18:28:13 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace itla3divisible
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int number;
+
+			Console.WriteLine("Bienvenido!");
+			Console.WriteLine("ingrese numero");
+
+			number = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("---------------------");
+			if (number % 3 == 0) {
+				Console.WriteLine(number + " es divisible por 3");
+			} else {
+				Console.WriteLine(number + " no es divisible por 3");
+			}
+			
+			if (number % 5 == 0) {
+				Console.WriteLine(" y es divisible por 5");
+			} else {
+				Console.WriteLine(" y no es divisible por 5");
+			}
+		}
+	}
+}
